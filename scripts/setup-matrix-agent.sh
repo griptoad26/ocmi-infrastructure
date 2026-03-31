83576c355786a80a725baf721b3232bfd37dc68c
griptoad26 <griptoad.26@gmail.com>
2026-03-31 16:26:44 -0700

    Update setup-matrix-agent.sh to use 100.64.0.1:8889

--- scripts/setup-matrix-agent.sh
+++ scripts/setup-matrix-agent.sh
@@ -3,9 +3,10 @@
 # Run this on each machine to connect to Matrix
 
 MACHINE_NAME=$(hostname)
-CONFIG_URL="http://192.168.50.174:8090/api/files/matrix-configs/${MACHINE_NAME}.json"
+CONFIG_URL="http://100.64.0.1:8889/matrix-configs/${MACHINE_NAME}.json"
 
 echo "Setting up Matrix agent for: $MACHINE_NAME"
+echo "Downloading from: $CONFIG_URL"
 
 # Create config directory
 mkdir -p ~/.openclaw/memory
